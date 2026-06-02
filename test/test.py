6b050a20e1478d9e715a5ea86429a893d0b6fc03
CambridgeinstituteoftechnologyBLR-Likitha <likitha.24ece@cambridge.edu.in>
2026-06-02 18:45:29 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -8,33 +8,45 @@ from cocotb.triggers import ClockCycles
 
 @cocotb.test()
 async def test_project(dut):
-    dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
+    dut._log.info("Starting TRNG Test")
+
     clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
 
     # Reset
-    dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
+
     await ClockCycles(dut.clk, 10)
+
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    # Test Input
+    dut.ui_in.value = 0b10101100
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    # Expected output according to project.v logic
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    feedback_path_a = ((0b10101100 >> 7) & 1) ^ ((0b10101100 >> 5) & 1)
+    feedback_path_b = ((0b10101100 >> 3) & 1) ^ ((0b10101100 >> 1) & 1)
+
+    expected = 0
+
+    for i in range(8):
+        bit = (0b10101100 >> i) & 1
+
+        if i % 2 == 0:
+            newbit = bit ^ feedback_path_a
+        else:
+            newbit = bit ^ feedback_path_b
+
+        expected |= (newbit << i)
+
+    dut._log.info(f"Expected Output = {expected}")
+    dut._log.info(f"Actual Output   = {int(dut.uo_out.value)}")
+
+    assert int(dut.uo_out.value) == expected
