9c2cfcd03e36468264ddb558ab9a5b47fa15a463
CambridgeinstituteoftechnologyBLR-Likitha <likitha.24ece@cambridge.edu.in>
2026-06-02 15:35:46 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,8 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_likitha_trng (
+  // Replace tt_um_example with your module name:
+tt_um_likitha_trng dut ( (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
