0b59858b56dce71b7c338aeefccd519441245242
CambridgeinstituteoftechnologyBLR-Likitha <likitha.24ece@cambridge.edu.in>
2026-06-02 14:26:49 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,27 +1,45 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2026 Likitha
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module tt_um_likitha_trng (
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+wire feedback_path_a;
+wire feedback_path_b;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+assign feedback_path_a =
+       ui_in[7] ^
+       ui_in[5];
+
+assign feedback_path_b =
+       ui_in[3] ^
+       ui_in[1];
+
+assign uo_out[0] = ui_in[0] ^ feedback_path_a;
+assign uo_out[1] = ui_in[1] ^ feedback_path_b;
+assign uo_out[2] = ui_in[2] ^ feedback_path_a;
+assign uo_out[3] = ui_in[3] ^ feedback_path_b;
+assign uo_out[4] = ui_in[4] ^ feedback_path_a;
+assign uo_out[5] = ui_in[5] ^ feedback_path_b;
+assign uo_out[6] = ui_in[6] ^ feedback_path_a;
+assign uo_out[7] = ui_in[7] ^ feedback_path_b;
+
+assign uio_out = 8'b0;
+assign uio_oe  = 8'b0;
+
+wire _unused = &{ena, clk, rst_n, uio_in, 1'b0};
 
 endmodule
